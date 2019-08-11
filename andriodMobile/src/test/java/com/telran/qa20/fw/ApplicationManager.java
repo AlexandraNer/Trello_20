88612ddd983cba624783428b9a9be2c07b326c9b
AlexandraNer <51996302+AlexandraNer@users.noreply.github.com>
2019-08-12 00:18:03 +0300

    Initial commit

---
+++ andriodMobile/src/test/java/com/telran/qa20/fw/ApplicationManager.java
@@ -0,0 +1,28 @@
+package com.telran.qa20.fw;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ApplicationManager {
+    AppiumDriver driver;
+    public void init() throws MalformedURLException {
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("deviceName", "qa20_class");
+        caps.setCapability("platformVersion", "8.0");
+        caps.setCapability("automationName", "Appium");
+        caps.setCapability("appPackage", "com.trello");
+        caps.setCapability("appActivity", ".home.HomeActivity");
+        caps.setCapability("app", "C:/Users/alex/Documents/alexandra/QA-20/Automation/com.trello_12562_apps.evozi.com.apk");
+
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), caps);
+    }
+    public void stop() {
+    }
+}
---
+++ andriodMobile/src/test/java/com/telran/qa20/tests/StartApplicationTest.java
@@ -0,0 +1,11 @@
+package com.telran.qa20.tests;
+
+import org.testng.annotations.Test;
+
+public class StartApplicationTest extends TestBase
+{
+    @Test
+    public void startAppTest(){
+        System.out.println("app started");
+    }
+}
